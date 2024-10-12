6db262b5b27861e77efccb457df39bb3ee2c0023
erc83 <espinoza.eric@gmail.com>
2024-10-12 01:01:18 -0300

    add: endpoint get and post /api/category basic

---
+++ src/presentation/category/controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response} from 'express'
+import { CustomError } from '../../domain';
+
+
+
+
+export class CategoryController {
+
+    //DI
+    constructor() {}
+
+     // manejo del error
+    private handleError = (error: unknown, res: Response ) => {
+        if ( error instanceof CustomError ) {
+            return res.status( error.statusCode).json({ error: error.message });
+        }
+        
+        console.log(`${ error }`)
+        return res.status(500).json({ error: 'Internal server error '})
+    }
+
+
+    createCategory =  async (req: Request, res: Response) => {
+
+        res.json('Create Category')
+
+    }
+
+    getCategory =  async (req: Request, res: Response) => {
+
+        res.json('Get Category')
+
+    }
+
+
+}
---
+++ src/presentation/category/routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express'
+import { CategoryController } from './controller'
+
+
+export class CategoryRoutes {
+    
+    static get routes(): Router {
+        
+        const router = Router()
+        const controller = new CategoryController()
+
+
+
+        // definir las rutas
+        router.get('/', controller.getCategory)
+        router.post('/', controller.createCategory )
+        
+
+        return router
+    }
+}
