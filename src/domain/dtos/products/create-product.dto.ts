ae39f2e221ee7584c59eccdf7fd37132e2c01455
erc83 <espinoza.eric@gmail.com>
2024-10-15 01:34:14 -0300

    add: create product dto

---
+++ src/domain/dtos/products/create-product.dto.ts
@@ -0,0 +1,41 @@
+
+
+export class CreateProductDto {
+
+    private constructor(
+        public readonly name: string,
+        public readonly available: boolean,
+        public readonly price: number,
+        public readonly description: string,
+        public readonly user: string,   // ID
+        public readonly category: string, // ID
+    ){}
+
+    static create( object: { [ key: string ]: any } ): [string?, CreateProductDto?] {
+
+        const {
+            name,
+            available,
+            price,
+            description,
+            user,
+            category,
+        } = object
+        
+        if( !name ) return ['Missing name']
+        if( !user ) return ['Missing user']
+        if( !category ) return ['Missing category']
+        
+        return [
+            undefined, 
+            new CreateProductDto(
+                name,
+                !!available,    //si viene un valor se considera true, si viene falso hago doble negacion y queda falso
+                price,
+                description,
+                user,
+                category,
+            )
+        ]
+    }
+}
